45938d12343529082725352679e4cb13c62d24c3
Russell Leong <rleong@udel.edu>
2016-03-02 09:30:05 -0500

    UPdate 2

--- CISC181.Lab2/src/main/java/pokerBase/Card.java
+++ CISC181.Lab2/src/main/java/pokerBase/Card.java
@@ -1,25 +1,62 @@
 package pokerBase;
 
+import java.util.Comparator;
 import pokerEnums.eRank;
 import pokerEnums.eSuit;
 
-public class Card {
+public class Card implements Comparable{
 
-	private eRank Rank;
-	private eSuit Suit;
+	private eRank eRank;
+	private eSuit eSuit;
+	private int iCardNbr;
 
-	public Card(eSuit Suit, eRank Rank) {
-		this.Rank = Rank;
-		this.Suit = Suit;
+	public Card(eSuit eSuit, eRank eRank, int iCardNbr) {
+		super();
+		this.eRank = eRank;
+		this.eSuit = eSuit;
+		this.iCardNbr = iCardNbr;
 	}
 	
-	public eRank getRank(){
-		return Rank;
+	public int getiCardNbr() {
+		return iCardNbr;
+	}
+
+	private void setiCardNbr(int iCardNbr) {
+		this.iCardNbr = iCardNbr;
+	}
+
+	private void seteRank(eRank eRank) {
+		this.eRank = eRank;
+	}
+
+	private void seteSuit(eSuit eSuit) {
+		this.eSuit = eSuit;
+	}
+
+	public eRank geteRank(){
+		return eRank;
 	}
 	
-	public eSuit getSuit(){
-		return Suit;
+	public eSuit geteSuit(){
+		return eSuit;
 	}
+	
+	public static Comparator<Card> CardRank = new Comparator<Card>(){
+		public int compare(Card c1, Card c2) {
+			int Cno1 = c1.geteRank().getiRankNbr();
+			int Cno2 = c2.geteRank().getiRankNbr();
+			
+			/*For descending order*/
+			return Cno2 - Cno1;
+		}
+	};
+	
+	@Override
+	public int compareTo(Object o){
+		Card c = (Card) o;
+		return c.getRank().compareTo(this.Rank);
+	}
+	
 	@Override
 	public String toString() {
 		return String.valueOf(Rank) + " OF " + String.valueOf(Suit);
--- CISC181.Lab2/src/main/java/pokerBase/Hand.java
+++ CISC181.Lab2/src/main/java/pokerBase/Hand.java
@@ -2,6 +2,7 @@ package pokerBase;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
@@ -25,83 +26,7 @@ public class Hand {
 	public Hand() {
 
 	}
+	
 
-	/*public ArrayList<Card> getCardsInHand() {
-		return CardsInHand;
-	}
-
-	public void setCardsInHand(ArrayList<Card> cardsInHand) {
-		CardsInHand = cardsInHand;
-	}
-
-	public ArrayList<Card> getBestCardsInHand() {
-		return BestCardsInHand;
-	}
-
-	public void setBestCardsInHand(ArrayList<Card> bestCardsInHand) {
-		BestCardsInHand = bestCardsInHand;
-	}
-
-	public HandScore getHandScore() {
-		return HandScore;
-	}
-
-	public void setHandScore(HandScore handScore) {
-		HandScore = handScore;
-	}
-
-	public boolean isbScored() {
-		return bScored;
-	}
-
-	public void setbScored(boolean bScored) {
-		this.bScored = bScored;
-	}
-
-	public boolean isFlush() {
-		return Flush;
-	}
-
-	public void setFlush(boolean flush) {
-		Flush = flush;
-	}
-
-	public boolean isStraight() {
-		return Straight;
-	}
-
-	public void setStraight(boolean straight) {
-		Straight = straight;
-	}
-
-	public boolean isAce() {
-		return Ace;
-	}
-
-	public void setAce(boolean ace) {
-		Ace = ace;
-	}
-
-	public Hand AddCardToHand(Card c) {
-		CardsInHand.add(c);
-		return this;
-	}*/
-
-	/**
-	 * EvaluateHand is a static method that will score
-	 * 
-	 * @param h
-	 * @return
-	 */
-
-	/*
-	 * public static Hand EvaluateHand(Hand h){ if (h.getCardsInHand().size() !=
-	 * 5){
-	 * 
-	 * }
-	 * 
-	 * HandScore hs = new HandScore(); try{ Class<?> c =
-	 * Class.forName("pokerBase.Hand") } }
-	 */
 	
 }
